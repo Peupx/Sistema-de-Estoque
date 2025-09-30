1e7d95892fa3235e884041d68dd1e7bc55a95cea
Peupx <pedro.gs.061@gmail.com>
2025-09-30 15:39:41 -0300

    Added function adicionarEstoque

--- functions.py
+++ functions.py
@@ -3,11 +3,15 @@ def adicionarEstoque():
     produtos = {} # Dicionário vazio... até então
     
     while continuar == 1:
-        nome = input("Coloque o nome do produto: ")
-        quantidade = input("Coloque a quantidade desse produto: ")
-        # Novos valores são inseridos por meio do código abaixo, pegando os inputs e colocando no dicionário
-        produtos[nome] = quantidade
-        
+        nome = input("Coloque o nome do produto: ").capitalize()
+        # Pega o input se já não esiver no dicionário
+        if nome in produtos:
+            print("Inválido! Nome já consta na lista")
+        else:
+            quantidade = input("Coloque a quantidade desse produto: ")
+            produtos[nome] = quantidade
+            
+            
         print(produtos)
         
         continuar = input("Deseja continuar? (S/N)").upper()
